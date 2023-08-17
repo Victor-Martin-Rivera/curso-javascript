ddfb75fb311f813356878582fa33f64eb717b328
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-08-16 19:52:42 -0700

    Variables en javascript

---
+++ 02- Variables/js/01-app.js
@@ -0,0 +1,38 @@
+//Inicializar una variable con un valor
+var producto = "Monitor de 24 Pulgadas";
+console.log(producto);
+
+//Las variables se pueden reasignar
+producto = 'Monitor de 19 Pulgadas'
+
+//JavaScript es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20;
+
+//Se pueden inicializar sin valor y asignarlo despues
+var disponible;
+disponible = true
+
+disponible = false
+
+//Inicializar multiples variables.
+var categoria = 'Computadoras',
+    marca = "Marca Famosa",
+    calificacion = 5;
+
+//Las variables no pueden iniciar con números
+//var 99dias; X
+var dias99;
+
+//var 01_; X
+var _01;
+var _usuario;
+var __usuario;
+
+//CamelCase (primera letra en minuscula y la segunda en mayuscula)
+var nombreProductoCategoriaPrecio;
+
+//underScore es todo con minuscula (igual Snake aunque no es muy comun)
+var nombre_producto_categoria_precio;
+
+//Pascal case(La primera letra en mayuscula)
+var Producto
---
+++ 02- Variables/js/03-app.js
@@ -0,0 +1,8 @@
+//const no puede ser reasignado
+//con let puedes inicializarlo despues y se puede reutilizar
+const producto = 'Tablet';
+producto = "Monitor";
+
+//Se debe inicializar la variable const siempre
+const precio = 20;
+console.log(producto);
