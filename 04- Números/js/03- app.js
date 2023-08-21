a9f3c427f9d8fbc60c4827cbb8308b96ca5e7d8b
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-08-20 17:13:45 -0700

    Números en JavaScript

---
+++ 04- Números/js/03- app.js
@@ -0,0 +1,36 @@
+
+let resultado;
+
+//PI
+resutado = Math.PI;
+
+//Redondear
+resultado = Math.round(2.4);
+
+//Redondear hacia arriba
+resultado = Math.ceil(2.1);
+
+//Redondear hacia abajo
+resultado = Math.floor(2.9);
+
+//Raiz Cuadrada
+resultado = Math.sqrt(144);
+
+//Valor absoluto
+resultado = Math.abs(-500);
+
+// Potencia (8 ^ 3)
+resultado = Math.pow(8, 3);
+
+// Minimo (Saca el numero menor que es el -3)
+resultado = Math.min(3, 5 , 1, 12, -3);
+// Saca el maximo
+resultaado = Math.max(3, 5 ,1 ,12 , -3);
+
+// Aleatorio (Random)
+resultado = Math.random() * 20;
+
+//Aleatorio dentro de un rango... (numeros del 0 al 30)
+resultado = Math.floor(Math.random() * 3);
+
+console.log(resultado);
