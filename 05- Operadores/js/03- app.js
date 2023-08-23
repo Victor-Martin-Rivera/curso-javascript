5911744861c1f56b4396ec055bc6b00586a5487c
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-08-22 21:53:26 -0700

    Operadores en JavaScript

---
+++ 05- Operadores/js/03- app.js
@@ -0,0 +1,12 @@
+// Undefined
+let numero;
+
+// console.log(typeof(numero));
+
+// Null
+let numero2 = null;
+console.log(numero2);
+// console.log(typeof(numero2));
+
+//=== - Comparador estricto
+console.log(numero === numero2);
