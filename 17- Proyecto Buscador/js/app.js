ac5224cab8454647869dcfc11f697e6a7b23c7cf
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-09-27 14:51:13 -0700

    Proyecto Buscador de Autos Actualizado

--- 17- Proyecto Buscador/js/app.js
+++ 17- Proyecto Buscador/js/app.js
@@ -1,23 +1,86 @@
 // Variables
-const resultado = document.querySelector('#resultado')
+const marca = document.querySelector('#marca')
 const year = document.querySelector('#year')
+const minimo = document.querySelector('#minimo')
+const maximo = document.querySelector('#maximo')
+const puertas = document.querySelector('#puertas')
+const transmision = document.querySelector('#transmision')
+const color = document.querySelector('#color')
+
+// Contenedor para los resultados
+const resultado = document.querySelector('#resultado')
 
 const max = new Date().getFullYear()
 const min = max - 10
 
+// Generar un objeto con la búsqueda
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: '',
+}
 
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
     // Una vez que cargue el HTML mostramos la funcion
-    mostrarAutos() // Muestra los uatos al cargar
+    mostrarAutos(autos) // Muestra los uatos al cargar
 
     // Llena las opciones de años
     llenarSelect()
 })
 
+// Event listener para los select de búsqueda
+marca.addEventListener('change', e => {
+    datosBusqueda.marca = e.target.value
+
+    filtrarAuto()
+})
+
+year.addEventListener('change', e => {
+    datosBusqueda.year = parseInt(e.target.value)
+
+    filtrarAuto()
+})
+
+minimo.addEventListener('change', e => {
+    datosBusqueda.minimo = e.target.value
+
+    filtrarAuto()
+})
+
+maximo.addEventListener('change', e => {
+    datosBusqueda.maximo = e.target.value
+
+    filtrarAuto()
+})
+
+puertas.addEventListener('change', e => {
+    datosBusqueda.puertas = parseInt(e.target.value)
+
+    filtrarAuto()
+})
+
+transmision.addEventListener('change', e => {
+    datosBusqueda.transmision = e.target.value
+
+    filtrarAuto()
+})
+
+color.addEventListener('change', e => {
+    datosBusqueda.color = e.target.value
+
+    filtrarAuto()
+})
+
 // Funciones
-function mostrarAutos() {
+function mostrarAutos(autos) {
+    limpiarHTML() // Elimina el HTML previo al filtrar
+    
     autos.forEach( auto => {
         const autoHTML = document.createElement('p')
 
@@ -31,6 +94,13 @@ function mostrarAutos() {
     })
 }
 
+// Limpiar HTML
+function limpiarHTML(){
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild)
+    }
+}
+
 // Genera los años del select
 function llenarSelect(){
     
@@ -40,4 +110,87 @@ function llenarSelect(){
         opcion.textContent = i
         year.appendChild(opcion) // Agrega las opciones del año al Select
     }
+}
+
+// Función que filtra en base a la búsqueda
+function filtrarAuto(){
+    // Se usa una Función de alto nivel
+    const resultado = autos.filter(filtrarMarca).filter( filtrarYear).filter( filtrarMinimo).filter( filtrarMaximo).filter(filtrarPuertas).filter(filtrarColor).filter(filtrarTransmision)
+    
+    
+
+    if(resultado.length){
+        mostrarAutos(resultado)
+    } else {
+        noResultado()
+    }
+}
+
+// Función que muestra un mensaje si no hay resultados encontrados
+function noResultado(){
+
+    limpiarHTML()
+
+    const noResultado = document.createElement('div')
+    noResultado.classList.add('alerta', 'error')
+    noResultado.textContent = 'No Hay Resultados, Selecciona otra Opción'
+
+    resultado.append(noResultado)
+}
+
+// Como tenemos una función se pasa un parametro para cada uno
+function filtrarMarca(auto){
+    const { marca } = datosBusqueda
+    if(marca ){
+        return auto.marca === marca
+    }
+    return auto
+}
+
+function filtrarYear(auto){
+    const { year } = datosBusqueda
+    if( year ){
+        return auto.year === year
+    }
+    return auto
+}
+
+function filtrarMinimo(auto){
+    const { minimo } = datosBusqueda
+    if( minimo ){
+        return auto.precio >= minimo
+    }
+    return auto
+}
+
+function filtrarMaximo(auto){
+    const { maximo } = datosBusqueda
+    if( maximo ){
+        return auto.precio <= maximo
+    }
+    return auto
+}
+
+function filtrarPuertas(auto){
+    const { puertas } = datosBusqueda
+    if( puertas ){
+        return auto.puertas === puertas
+    }
+    return auto
+}
+
+function filtrarColor(auto){
+    const { color } = datosBusqueda
+    if( color ){
+        return auto.color === color
+    }
+    return auto
+}
+
+function filtrarTransmision(auto){
+    const { transmision } = datosBusqueda
+    if( transmision ){
+        return auto.transmision === transmision
+    }
+    return auto
 }
