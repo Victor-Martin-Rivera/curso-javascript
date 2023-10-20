82b118a22182b250eda7cd499a80153b58bb2c2e
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-10-20 13:14:44 -0700

    Proyecto API Clima Válidacion

---
+++ 36- Proyecto Api Clima/js/app.js
@@ -0,0 +1,54 @@
+
+const container = document.querySelector('.container')
+const resultado = document.querySelector('#resultado')
+const formulario = document.querySelector('#formulario')
+
+window.addEventListener('load', () => {
+  formulario.addEventListener('submit', buscarClima)
+})
+
+function buscarClima(e){
+  e.preventDefault();
+
+  // console.log('Buscando el Clima')
+  
+  // Validar
+  const ciudad = document.querySelector('#ciudad').value
+  const pais = document.querySelector('#pais').value
+
+  if(ciudad === '' || pais === ''){
+    // Hubo un error
+    mostrarError('Ambos campos son Obligatorios')
+
+    return
+  }
+
+  // Consultar la API
+}
+
+function mostrarError(mensaje){
+  // Estilos de Tailwind
+  const alerta = document.querySelector('.bg-red-100')
+  
+  // Si no hay una alerta entonces agregala(no se duplica)
+  if(!alerta)
+  {
+    // Crear una alerta
+    const alerta = document.createElement('div')
+      
+    alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center')
+
+    alerta.innerHTML = `
+      <strong class="font-bold">Error!</strong>
+      <span class="block">${mensaje}</span>
+      `
+
+      // Agregamos nuestra alerta al contenedor(HTML)
+      container.appendChild(alerta)
+
+      // La alerta desaparecera despues de 5 segundos
+      setTimeout(() => {
+        alerta.remove()
+      }, 5000);
+  }
+}
