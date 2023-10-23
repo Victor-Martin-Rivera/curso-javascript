16233d593c32dfe3caee6a40163fc5b33e25da94
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-10-22 18:33:23 -0700

    Proyecto Buscador de Recetas

---
+++ 37- Proyecto-BuscadorRecetas/js/app.js
@@ -0,0 +1,95 @@
+function iniciarApp(){
+
+    const selectCategorias = document.querySelector('#categorias')
+    const resultado = document.querySelector('#resultado')
+
+    selectCategorias.addEventListener('change', seleccionarCategoria)
+
+    obtenerCategorias()
+
+    function obtenerCategorias(){
+        const url = 'https://www.themealdb.com/api/json/v1/1/categories.php'
+        fetch(url)
+            .then( respuesta => respuesta.json())
+            .then( resultado => mostrarCategorias(resultado.categories))
+    }
+
+    function mostrarCategorias(categorias = []){
+        categorias.forEach( categoria => {
+
+            const { strCategory } = categoria
+            const option = document.createElement('OPTION');
+            option.value = strCategory
+            option.textContent = strCategory
+            selectCategorias.appendChild(option) 
+           
+        })
+    }
+
+    function seleccionarCategoria(e){
+        const categoria = e.target.value
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`
+        fetch(url)
+            .then( respuesta => respuesta.json())
+            .then( resultado => mostrarRecetas(resultado.meals))
+    }
+
+    function mostrarRecetas(recetas = []){
+        
+        // Limpia el HTML previo y carga nuevas recetas del select
+        limpiarHTML(resultado)
+
+        const heading = document.createElement('h2')
+        heading.classList.add('text-center', 'text-black', 'my-5')
+        heading.textContent = recetas.length ? 'Resultados' : 'No hay Resultados'
+        resultado.appendChild(heading)
+
+        // Iterar en los resultados
+        recetas.forEach( receta => {
+            const { idMeal, strMeal, strMealThumb } = receta
+
+            const recetaContenedor = document.createElement('DIV')
+            // Se mostrará en columnas
+            recetaContenedor.classList.add('col-md-4')
+            
+            const recetaCard = document.createElement('DIV')
+            recetaCard.classList.add('card', 'mb-4')
+
+            const recetaImagen = document.createElement('IMG')
+            recetaImagen.classList.add('card-img-top')
+            recetaImagen.alt = `Imagen de la Receta ${strMeal}`
+            recetaImagen.src = strMealThumb
+
+            const recetaCardBody = document.createElement('div')
+            recetaCardBody.classList.add('card-body')
+
+            const recetaHeading = document.createElement('h3')
+            recetaHeading.classList.add('card-title', 'mb-3')
+            recetaHeading.textContent = strMeal
+
+            const recetaButton = document.createElement('button')
+            recetaButton.classList.add('btn', 'btn-danger', 'w-100')
+            recetaButton.textContent = 'Ver Receta'
+            recetaButton.dataset.bsTarget = '#modal'
+            recetaButton.dataset.bsToggle = 'modal'
+
+            // Inyectar en el codigo HTML
+            recetaCardBody.appendChild(recetaHeading)
+            recetaCardBody.appendChild(recetaButton)
+
+            recetaCard.appendChild(recetaImagen)
+            recetaCard.appendChild(recetaCardBody)
+
+            recetaContenedor.appendChild(recetaCard)
+            resultado.appendChild(recetaContenedor)
+        })
+    }
+
+    function limpiarHTML(selector){
+        while(selector.firstChild){
+            selector.removeChild(selector.firstChild)
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', iniciarApp)
