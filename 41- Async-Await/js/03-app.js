9e2233bcc973069b4888f628ff4d269ae4f74bce
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-11-01 14:06:39 -0700

    Async - Await

---
+++ 41- Async-Await/js/03-app.js
@@ -0,0 +1,29 @@
+// Async Await con Function Declaration
+function descargarClientes(){
+
+    return new Promise((resolve, reject) => {
+        const error = false
+
+        setTimeout(() => {
+            if(!error) {
+                resolve('El Listado de Clientes se descargo correctamente')
+            } else {
+                reject('Error en la conexión')
+            }
+        }, 3000);
+    })
+}
+
+// Async await
+const ejecutar = async ()  => {
+    try {
+        console.log( 1 + 1)
+        const respuesta = await descargarClientes()
+
+        console.log( 2 + 2)
+        console.log(respuesta)
+    } catch (error) {
+        console.log(error)
+    }
+}
+ejecutar()
---
+++ 41- Async-Await/js/04-app.js
@@ -0,0 +1,32 @@
+function descargarNuevosClientes() {
+    return new Promise( resolve => {
+        console.log('Descargando Clientes....');
+        setTimeout( () => {
+            resolve('Los clientes fueron descargados');           
+        }, 5000)
+
+    })
+}
+
+function descargarNuevosPedidos() {
+    return new Promise( resolve => {
+        console.log('Descargando Pedidos....');
+        setTimeout( () => {
+            resolve('Los pedidos fueron descargados');           
+        }, 5000)
+    })
+}
+
+const app = async () => 
+{
+    try {
+        const respuesta = await Promise.all([descargarNuevosClientes() , descargarNuevosPedidos()])
+
+        console.log(respuesta[0])
+        console.log(respuesta[1])
+    } catch(error){
+        console.log(error)
+    }
+}
+
+app()
