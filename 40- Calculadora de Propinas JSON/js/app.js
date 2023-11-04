ade5e4e8b3e20729b047867cbeefc53855aac7eb
Victor Martin Rivera Moreno <al16171472@ite.edu.mx>
2023-11-03 19:58:08 -0700

    Calculadora de propinas proyecto

---
+++ 40- Calculadora de Propinas JSON/js/app.js
@@ -0,0 +1,64 @@
+let cliente = {
+    mesa: '',
+    hora: '',
+    pedido: []
+}
+
+const btnGuardarCliente = document.querySelector('#guardar-cliente')
+btnGuardarCliente.addEventListener('click', guardarCliente)
+
+function guardarCliente(){
+     const mesa = document.querySelector('#mesa').value
+     const hora = document.querySelector('#hora').value
+
+     // Revisar si hay campos vacios
+     const camposVacios = [ mesa, hora ].some( campo => campo === '')
+
+     if(camposVacios)
+     {
+        // Verificar si hay una alerta
+        const existeAlerta = document.querySelector('.invalid-feedback')
+        
+        if(!existeAlerta) 
+        {
+        const alerta = document.createElement('div')
+        alerta.classList.add('invalid-feedback', 'd-block', 'text-center')
+        alerta.textContent = 'Todos los campos son obligatorios'
+        document.querySelector('.modal-body form').appendChild(alerta)
+    
+            setTimeout(() => {
+                alerta.remove()
+            }, 3000)
+        }
+        return
+    }
+    
+    // Asignar datos del formulario a cliente
+    cliente = { ...cliente, mesa, hora }
+
+    // Ocultar modal
+    const modalFormulario = document.querySelector('#formulario')
+    const modalBootstrap = bootstrap.Modal.getInstance(modalFormulario)
+    modalBootstrap.hide()
+
+    // Mostrar las secciones
+    mostrarSecciones()
+
+    // Obtener Platillos de la API de JSON Server
+    obtenerPlatillos()
+
+}
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none')
+    seccionesOcultas.forEach( seccion => seccion.classList.remove('d-none'))
+}
+
+function obtenerPlatillos(){
+    const url = 'http://localhost:5500/platillos'
+
+    fetch(url)
+        .then( respuesta => console.log(respuesta))
+        .then( resultado => console.log(resultado))
+        .catch( error => console.log(error))
+}
